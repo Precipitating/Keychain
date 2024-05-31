49410bab80893b7346285ab29206854739637590
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-05-31 03:23:47 +0100

    Added template code that simply responds to certain phrases via DMS or server

--- main.py
+++ main.py
@@ -1,6 +1,61 @@
 from typing import Final
 import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
 
-def get_response(user_input: str) -> str:
-    raise NotImplementedError("Missing Code")
+# LOAD DISCORD TOKEN
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+# SET BOT
+intents: Intents = Intents.default()
+intents.message_content = True # NOQA
+client: Client = Client(intents=intents)
+
+# MESSAGE FUNCTIONALITY
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print("(Empty message due to no intents enabled)")
+        return
+
+    if is_private:= user_message[0] == '?':
+            user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+# HANDLE STARTUP
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+# HANDLE INCOMING MESSAGES
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}')
+    await send_message(message,user_message)
+
+# MAIN ENTRY POINT
+def main() -> None:
+    client.run(token=TOKEN)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
 
